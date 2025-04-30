adaa6bdf3bd46e39ed4e4f02f1938e02f38fedae
thaisab89 <thais.bertolino8@gmail.com>
2025-04-30 19:32:17 -0300

    Update exercicio_1.py

--- exercicio_1.py
+++ exercicio_1.py
@@ -25,3 +25,12 @@ Pode-se utilizar o comando "while" ou até o "for" para te ajudar nisso.
 
 Fonte: Curso em vídeo.
 """
+
+n = int(input('Digite um número: '))
+fatorial = 1
+for i in range(n):
+        f = n - i
+        if f > 0:
+                fatorial = fatorial * f
+
+print (fatorial)
